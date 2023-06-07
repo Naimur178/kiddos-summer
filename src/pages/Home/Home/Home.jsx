fcd4230577ec4627ce06c598d3aa6b7f3e06726a
Naimur178 <naim.cse17@gmail.com>
2023-06-07 13:02:44 +0600

    navBar and footer is added

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-center font-bolder text-4xl">Hello from home</h2>
+        </div>
+    );
+};
+
+export default Home;
