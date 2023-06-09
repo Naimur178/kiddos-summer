112853dd8cf5170e603be30aa0413fee21acd90e
Naimur178 <naim.cse17@gmail.com>
2023-06-09 09:36:02 +0600

    route set up

---
+++ src/pages/Courses/Courses.jsx
@@ -0,0 +1,11 @@
+
+
+const Courses = () => {
+    return (
+        <div>
+            <h2>Hello from </h2>
+        </div>
+    );
+};
+
+export default Courses;
--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -1,4 +1,4 @@
-
+import 'swiper/css/swiper.css'
 
 const Featured = () => {
     return (
